cc755d8af298da50f4c5a824d465adde27abef85
Mariana Matos <mmatos@gmail.com>
2020-01-11 20:48:42 -0300

    Adding faker to create test animals

--- server/test/animals.test.ts
+++ server/test/animals.test.ts
@@ -5,6 +5,7 @@ import Animal from '../../model/Animal'
 import server from '../src/server'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import { MongoClient } from 'mongodb'
+import faker from 'faker'
 
 const mongoServer = new MongoMemoryServer({ debug: true })
 let mongoClient: MongoClient
@@ -21,12 +22,37 @@ const instance = axios.create({ baseURL: 'http://localhost:8080' })
 
 should()
 
+const createAnimal = (delta: Partial<Animal>): Animal => {
+  return {
+    name: faker.name.findName(),
+    species: faker.random.arrayElement(['dog', 'cat']),
+    gender: faker.random.arrayElement(['M', 'F']),
+    age: faker.random.number(99),
+    size: faker.random.arrayElement(['S', 'M', 'L']),
+    info: faker.lorem.sentence(),
+    image: faker.image.imageUrl(),
+    ...delta
+  }
+}
+const save = <T>(collectionName: string) => async (...objectsToSave: T[]) => {
+  const collection = mongoClient.db('patitas-test').collection<T>(collectionName)
+  for (const objectToSave of objectsToSave) {
+    await collection.save({ ...objectToSave })
+  }
+}
+
+const withoutId = (obj: { _id?: string }) => {
+  const { _id, ...response } = obj
+  return response
+}
+
 describe('Animals API', () => {
   it('GET /animals should return a list including animals in the db', async () => {
-    const animalsCollection = mongoClient.db('patitas-test').collection<Animal>('animals')
-    await animalsCollection.save({ name: 'pupi', species: 'dog', gender: 'M', age: 33, size: 'M', info: '', image: '' })
+    const animal = createAnimal({ name: 'pupi' })
+    await save<Animal>('animals')(animal)
     const response = await instance.get<Animal[]>('/animals')
-    response.data[0].name.should.be.equal('pupi')
+    withoutId(response.data[0]).should.be.deep.equal(animal)
   })
 })
+
 
